96264338594f74ef1252b46d4939415f27ae4826
ArchambaultP <ehras@hotmail.com>
2019-09-24 10:39:56 -0400

    Project Setup

---
+++ Comp354Project/src/main/java/com/comp354project/Comp354Project/Comp354ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.comp354project.Comp354Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp354ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp354ProjectApplication.class, args);
+	}
+
+}
---
+++ Comp354Project/src/test/java/com/comp354project/Comp354Project/Comp354ProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.comp354project.Comp354Project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Comp354ProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
