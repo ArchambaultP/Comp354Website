f6298c23f1b7ccf88be26ffa4c0e017de3306b4b
DeeAndre Mac Andrew <34717537+ConcernedAnt@users.noreply.github.com>
2019-10-16 16:55:29 -0400

    fml

--- Comp354Project/Project-UI/src/app/app-routing.module.ts
+++ Comp354Project/Project-UI/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './Components/user-list/user-list.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegistrationComponent } from './Components/register/registration.component';
+import {ProductPageComponent} from "./Components/product-page/product-page.component";
+import {ProductDetailsComponent} from "./Components/product-details/product-details.component";
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'registration', component: RegistrationComponent}
+  { path: 'registration', component: RegistrationComponent},
+  { path: 'users', component: UserListComponent },
+  { path: 'products', component: ProductPageComponent},
+  { path: 'products/:productId', component: ProductDetailsComponent},
 ];
 
 @NgModule({
