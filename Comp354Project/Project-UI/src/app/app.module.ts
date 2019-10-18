78145bc815af430226699fb22f63097c905a6c4d
JAY <izzo.j@hotmail.com>
2019-10-17 22:41:35 -0400

    products organized in a row and display filter working

--- Comp354Project/Project-UI/src/app/app.module.ts
+++ Comp354Project/Project-UI/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { AccountFormComponent } from './Components/admin/account-form/account-fo
 import { AccountUpdateComponent } from './Components/admin/account-update/account-update.component';
 import { AccountDetailsComponent } from './Components/admin/account-details/account-details.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
