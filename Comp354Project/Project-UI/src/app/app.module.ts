488ee53138593bea23c64a49998aafa0edac3c1e
JAY <izzo.j@hotmail.com>
2019-10-17 22:41:35 -0400

    products organized in a row and display filter working

--- Comp354Project/Project-UI/src/app/app.module.ts
+++ Comp354Project/Project-UI/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ProductPageComponent } from './Components/product-page/product-page.com
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 import { RelatedItemsComponent } from './components/related-items/related-items.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
