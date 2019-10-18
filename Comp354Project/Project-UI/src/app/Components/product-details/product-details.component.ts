2f49912a7111ecf5add3d4c4d22ba1cecaaa3324
JAY <izzo.j@hotmail.com>
2019-10-17 22:41:35 -0400

    products organized in a row and display filter working

--- Comp354Project/Project-UI/src/app/Components/product-details/product-details.component.ts
+++ Comp354Project/Project-UI/src/app/Components/product-details/product-details.component.ts
@@ -8,14 +8,24 @@ import { ActivatedRoute} from '@angular/router';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+
   products = products;
   product;
   constructor(private route: ActivatedRoute,) {
     }
+<<<<<<< HEAD
+=======
+    
+>>>>>>> products organized in a row and display filter working
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId') - 1];
     });
   }
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> products organized in a row and display filter working
 }
