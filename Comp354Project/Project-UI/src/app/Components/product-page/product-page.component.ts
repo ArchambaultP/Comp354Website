31732acd0db5a602796de55f57ac59a329a7c9cd
JAY <izzo.j@hotmail.com>
2019-10-17 22:41:35 -0400

    products organized in a row and display filter working

--- Comp354Project/Project-UI/src/app/Components/product-page/product-page.component.ts
+++ Comp354Project/Project-UI/src/app/Components/product-page/product-page.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { products} from "../../products";
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { products } from "../../products";
+import { ascend, filter, groupBy, pipe, sort } from "ramda";
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
-products = products;
-  constructor() { }
+  products = products;
+
+//this section sorts out products on the page
+  selectedRadioButtonValue: any = 1;
+
+
+
+  @Output()
+  countRadioButtonSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  onRadioButtonSelectionChange(selectedRadioButtonValue: any): void{
+    this.countRadioButtonSelectionChanged.emit(this.selectedRadioButtonValue);
+    console.log(this.selectedRadioButtonValue);
+
+    if(this.selectedRadioButtonValue == 1){
+      this.products = sort(ascend(products => products.name), products)
+      console.log("going through name");
+    }
+    else{
+      this.products = sort(ascend(products => products.price), products)
+      console.log("going through price");
+    }
+
+  }
+
+
+
+
 
   ngOnInit() {
   }
