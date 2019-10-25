d7e7c20f8c388385c654ef48b328218e63018df9
AyushKharade <ayush.kharade999@gmail.com>
2019-10-25 11:41:57 -0400

    added routing links for each button
    rn they all route to users component, once we have other pages and other components we just modfy the routing file

--- Comp354Project/Project-UI/src/app/app-routing.module.ts
+++ Comp354Project/Project-UI/src/app/app-routing.module.ts
@@ -3,7 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './Components/user-list/user-list.component';
 
 const routes: Routes = [
-  { path: 'users', component: UserListComponent }
+  { path: 'users', component: UserListComponent },
+  { path: 'register', component: UserListComponent },
+  { path: 'login', component: UserListComponent },
+  { path: 'search', component: UserListComponent },
+  { path: 'cart', component: UserListComponent },
+  { path: 'checkout', component: UserListComponent },
+  { path: 'trending', component: UserListComponent },
+  { path: 'popular', component: UserListComponent },
+  { path: 'new', component: UserListComponent },
+  { path: 'recent', component: UserListComponent },
+  { path: 'nextpage', component: UserListComponent },
+  { path: 'prevpage', component: UserListComponent }
 ];
 
 @NgModule({
