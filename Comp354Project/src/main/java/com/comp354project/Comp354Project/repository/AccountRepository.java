8cb439d18a9e3ab85f944c4ff6ba9fb48c2dcbbb
Hau Gilles Che <haugillesche@gmail.com>
2019-10-25 17:48:49 -0400

    Changed return type of findByEmail to a list to comply template

--- Comp354Project/src/main/java/com/comp354project/Comp354Project/repository/AccountRepository.java
+++ Comp354Project/src/main/java/com/comp354project/Comp354Project/repository/AccountRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface AccountRepository extends CrudRepository<Account, Integer> {
-    public Account findByEmail(String email);
+    List<Account> findByEmail(String email);
+   // public List<Account> findAll();
 }
