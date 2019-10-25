c70fb696511137377b28927d995b5e480fe6a4af
Hau Gilles Che <haugillesche@gmail.com>
2019-10-25 17:35:53 -0400

    Front-end Login implementation

---
+++ Comp354Project/Project-UI/src/app/service/auth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
+    public email: String;
+    public password: String;
+
+    constructor(private http: HttpClient){}
+
+    login(credentials): Observable<any>{
+        if(credentials != undefined){
+            const body = new HttpParams()
+                .set('email',credentials.email)
+                .set('password',credentials.password);
+
+            return this.http.post('http://localhost:8080/auth/login',
+                 body.toString(),
+                     {
+                         headers: new HttpHeaders()
+                             .set('Content-Type','application/x-www-form-urlencoded')
+                     }
+                 );
+        }
+    }
+
+    registerSuccessfulLogin(email, password){
+        sessionStorage.setItem(this.USER_NAME_SESSION_ATTRIBUTE_NAME, email);
+    }
+
+    logout(){
+        sessionStorage.removeItem(this.USER_NAME_SESSION_ATTRIBUTE_NAME);
+        this.email = null;
+        this.password = null;
+    }
+
+    currentUser(){
+        const user = sessionStorage.getItem(this.USER_NAME_SESSION_ATTRIBUTE_NAME);
+        if(user){
+            return sessionStorage.getItem(this.USER_NAME_SESSION_ATTRIBUTE_NAME);
+        }
+    }
+
+    isUserLoggedIn(){
+        let user = sessionStorage.getItem(this.USER_NAME_SESSION_ATTRIBUTE_NAME)
+        return user;
+    }
+}
