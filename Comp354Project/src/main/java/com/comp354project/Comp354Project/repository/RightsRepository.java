89265a7e67bb4a9d1573b80fdc6ae8830ae706e6
Igniswolf <43484136+Igniswolf10@users.noreply.github.com>
2019-10-25 15:56:01 -0400

    added seed, removed rights and added flags to account

--- Comp354Project/src/main/java/com/comp354project/Comp354Project/repository/RightsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.comp354project.Comp354Project.repository;
-
-import com.comp354project.Comp354Project.Entities.Rights;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RightsRepository extends CrudRepository<Rights, Integer> {
-    public Rights findByName(String name);
-}
