d71d7400b1e14b4f1d85fdbd039b480c68e0b632
Hau Gilles Che <haugillesche@gmail.com>
2019-10-25 17:42:22 -0400

    Removed spring security dependency & added simple json dependency

--- Comp354Project/Project-UI/src/app/app-routing.module.ts
+++ Comp354Project/Project-UI/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './Components/user-list/user-list.component';
+import { LoginComponent } from './Components/login/login.component';
 
 const routes: Routes = [
-  { path: 'users', component: UserListComponent }
+  { path: 'users', component: UserListComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
