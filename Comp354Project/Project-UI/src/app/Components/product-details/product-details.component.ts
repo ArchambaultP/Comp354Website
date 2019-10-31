91a40d72c9afd3dcd3deb2ed9bf088e6889aeca6
DeeAndre Mac Andrew <34717537+ConcernedAnt@users.noreply.github.com>
2019-10-31 18:10:57 -0400

    fixed finding item in product array

--- Comp354Project/Project-UI/src/app/Components/product-details/product-details.component.ts
+++ Comp354Project/Project-UI/src/app/Components/product-details/product-details.component.ts
@@ -11,34 +11,19 @@ import { Product } from '../../model/product';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-<<<<<<< HEAD
-
-=======
->>>>>>> Fixed product page bug when sorted
   products = products;
   product;
+
   constructor(private route: ActivatedRoute,) {
-    }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    
->>>>>>> products organized in a row and display filter working
-=======
->>>>>>> Fixed product page bug when sorted
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
-      //params.get('productID') gets productId from the route
-      this.product = products[+params.get('productId') - 1];
+      this.product = products.find(product => product.productId === parseInt(params.get('productId')));
     });
   }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
-
->>>>>>> products organized in a row and display filter working
-=======
->>>>>>> Fixed product page bug when sorted
+  addToCart() {
+    window.alert('Item added to cart');
+  }
 }
