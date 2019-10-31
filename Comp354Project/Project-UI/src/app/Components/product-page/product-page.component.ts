f15d175a1d642d3b3c28edee509a653f0962fae8
JAY <izzo.j@hotmail.com>
2019-10-31 18:08:45 -0400

    working dropdown sorting for product page items

--- Comp354Project/Project-UI/src/app/Components/product-page/product-page.component.ts
+++ Comp354Project/Project-UI/src/app/Components/product-page/product-page.component.ts
@@ -1,76 +1,44 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { products } from "../../products";
 import { ascend, filter, groupBy, pipe, sort } from "ramda";
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> products organized in a row and display filter working
-=======
 
->>>>>>> Fixed product page bug when sorted
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
-  products = products;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> Fixed product page bug when sorted
+
+  @Output() refresh:EventEmitter<string> = new EventEmitter();
+
+  products = sort(ascend(products => products.name), products);
 
 //this section sorts out products on the page
-  selectedRadioButtonValue: any = 1;
-
+  selectedDropDownButtonValue: any = 1;
   @Output()
-  countRadioButtonSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
+  countDropDownButtonSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
 
-  onRadioButtonSelectionChange(selectedRadioButtonValue: any): void{
-    this.countRadioButtonSelectionChanged.emit(this.selectedRadioButtonValue);
-    console.log(this.selectedRadioButtonValue);
+  onDropDownButtonSelectionChange(selectedDropDownButtonValue: any): void{
+    this.countDropDownButtonSelectionChanged.emit(this.selectedDropDownButtonValue);
+    console.log(this.selectedDropDownButtonValue);
 
-    if(this.selectedRadioButtonValue == 1){
+    if(this.selectedDropDownButtonValue == 1){
       this.products = sort(ascend(products => products.name), products)
-      console.log("going through name");
-      console.log(products)
+    }
+    else if(this.selectedDropDownButtonValue == 2){
+      this.products = sort(ascend(products => products.name), products).reverse()
+    }
+    else if(this.selectedDropDownButtonValue == 3){
+      this.products = sort(ascend(products => products.price), products)
     }
     else{
-      this.products = sort(ascend(products => products.price), products)
-      console.log("going through price");
+      this.products = sort(ascend(products => products.price), products).reverse()
     }
+
   }
-<<<<<<< HEAD
 
 
-
-
->>>>>>> products organized in a row and display filter working
-
-//this section sorts out products on the page
-  selectedRadioButtonValue: any = 1;
-
-  @Output()
-  countRadioButtonSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
-
-  onRadioButtonSelectionChange(selectedRadioButtonValue: any): void{
-    this.countRadioButtonSelectionChanged.emit(this.selectedRadioButtonValue);
-    console.log(this.selectedRadioButtonValue);
-
-    if(this.selectedRadioButtonValue == 1){
-      this.products = sort(ascend(products => products.name), products)
-      console.log("going through name");
-      console.log(products)
-    }
-    else{
-      this.products = sort(ascend(products => products.price), products)
-      console.log("going through price");
-    }
-  }
-=======
->>>>>>> Fixed product page bug when sorted
   ngOnInit() {
   }
 
