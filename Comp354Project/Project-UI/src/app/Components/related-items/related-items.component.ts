a43689aa2d4049ab33a81cf42243dffe4f8924a0
JonathanFrey2704 <38487829+JonathanFrey2704@users.noreply.github.com>
2019-11-02 19:20:49 -0400

    added ramda

--- Comp354Project/Project-UI/src/app/Components/related-items/related-items.component.ts
+++ Comp354Project/Project-UI/src/app/Components/related-items/related-items.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@Angular/core';
 import {products} from "../../products";
 import {Input} from "@angular/core";
 
