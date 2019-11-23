f7e7abbe6e01ef182d3add732a8171e69738df92
Hau Gilles Che <haugillesche@gmail.com>
2019-11-23 15:23:01 -0500

    Converted login & registration pages into modals

--- Comp354Project/Project-UI/src/app/Components/login/login.component.ts
+++ Comp354Project/Project-UI/src/app/Components/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthenticatedUser } from '../../model/authenticatedUser';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
@@ -18,7 +19,7 @@ export class LoginComponent implements OnInit{
     loginSuccess = false;
     authUser: AuthenticatedUser;
 
-    constructor(private auth: AuthService, private http: HttpClient, private router: Router){
+    constructor(private auth: AuthService, private http: HttpClient, private router: Router, public activeModal: NgbActiveModal){
         if(this.auth.isUserLoggedIn()){
             this.router.navigate(['/']);
         }
@@ -36,7 +37,7 @@ export class LoginComponent implements OnInit{
                             this.authUser = data;
                             this.auth.registerSuccessfulLogin(this.authUser);
                             this.loginSuccess = true;
-                            this.router.navigate(['/']);
+                            this.activeModal.close('Login Success');
                         }else{
                             this.errorMsg=data['error'];
                             this.invalidLogin= true;
