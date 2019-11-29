a17db9f9c30a5fa99f4e436bd92a949bda33ca93
Luciano <lucianoporchet@gmail.com>
2019-11-29 16:56:54 -0500

    fixed bugs

--- Comp354Project/Project-UI/src/app/Components/product-form/product-form.component.ts
+++ Comp354Project/Project-UI/src/app/Components/product-form/product-form.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ProductService } from '../../service/product.service';
+import { AuthService } from '../../service/auth.service';
 import { Product } from '../model/product';
+import { AuthenticatedUser } from '../model/authenticatedUser';
 
 
 @Component({
@@ -12,15 +14,18 @@ import { Product } from '../model/product';
 })
 export class ProductFormComponent implements OnInit {
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService, private authService: AuthService) {
 
     this.productService.findAllCategories().subscribe(data=> {
           for(let item of data){
             this.categories.push(item.name);
           }
         });
+
+    this.user = this.authService.currentUser();
   }
 
+  user: AuthenticatedUser;
   categories = [];
   submitted = false;
   selectedFile: File = null;
@@ -37,30 +42,9 @@ export class ProductFormComponent implements OnInit {
     this.submitted = true;
     console.log(value);
 
-
-
-    //const fd = new FormData();
-    //fd.append('image', this.selectedFile, this.selectedFile.name);
-    //this.http.post('', fd).subscribe(res => {console.log(res)});
   }
 
 
-  onFileSelected(files){
-    //this.selectedFile = <File>event.target.files[0];
-    if (files.length === 0)
-        return;
 
-    var mimeType = files[0].type;
-    if (mimeType.match(/image\/*/) == null) {
-        this.message = "Only images are supported.";
-            return;
-    }
-    var reader = new FileReader();
-    this.imagePath = files;
-    reader.readAsDataURL(files[0]);
-    reader.onload = (_event) => {
-        this.imgURL = reader.result;
-    }
-  }
 
 }
