6e790399820383310522becf76537b0b635fadde
JonathanFrey2704 <38487829+JonathanFrey2704@users.noreply.github.com>
2019-12-03 17:47:47 -0500

    added shuffle to products

--- Comp354Project/Project-UI/src/app/Components/product-page/product-page.component.ts
+++ Comp354Project/Project-UI/src/app/Components/product-page/product-page.component.ts
@@ -164,4 +164,20 @@ minFilter = null;
      this.maxFilter = null;
      this.minFilter = null;
    }
+
+  shuffleArray(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+return array;
+}
 }
